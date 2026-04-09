920669f143be8e87605f778b90215f189fec7200
prabir <prabirrahman1@gmail.com>
2026-04-09 19:40:50 +0530

    fix base path 1

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,19 @@ import Education from "./Pages/Education";
 import Skill from "./Pages/Skill";
 import Weather from "./Pages/Weather";
 
-
-
 const App = () => {
   return (
     <div className="h-screen flex flex-col overflow-hidden bg-gradient-to-b from-black via-blue-950 to-black text-white">
       <Navbar />
-     <div className="flex-1 flex items-center justify-center overflow-auto">
-       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Education" element={<Education />} />
-        <Route path="/Skill" element={<Skill />} />
-        <Route path="/Weather" element={<Weather />} />
-      </Routes>
-     </div>
+      <div className="flex-1 flex items-center justify-center overflow-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Education" element={<Education />} />
+          <Route path="/Skill" element={<Skill />} />
+          <Route path="/Weather" element={<Weather />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
